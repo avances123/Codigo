049cf08cd8128fe96fe7815b7f095c2bc190159c
avances123 <avances123@gmail.com>
2010-09-05 01:13:31 +0200

    test3 y test4 completados

--- python/encontrarse/individuo.py
+++ python/encontrarse/individuo.py
@@ -4,13 +4,29 @@ import random
 
 class Individuo:
 
- 
+    def moverAContigua(self):
+	exito=False
+	while exito==False:
+    	    incx = random.choice([-1,0,1])
+ 	    incy = random.choice([-1,0,1])
+	    if self.x+incx > 0 and self.x+incx <= self.borde and self.y+incy > 0 and self.y+incy <= self.borde:
+		exito=True
+            if incx == 0 and incy == 0:
+                exito=False
+   	# Aplicamos el incremento
+        self.x += incx
+        self.y += incy
+        self.movimientos += 1
+
+
+
     def moverACualquiera(self):
-        self.x=random.randrange(1,100)   
-        self.y=random.randrange(1,100)   
+        self.x=random.randrange(1,self.borde)   
+        self.y=random.randrange(1,self.borde)   
 	self.movimientos += 1
 	
-    def __init__(self,x=random.randrange(1,100),y=random.randrange(1,100)):
+    def __init__(self,x,y,borde=100):
         self.x=x
         self.y=y
 	self.movimientos=0
+	self.borde=borde
--- python/encontrarse/test.py
+++ python/encontrarse/test.py
@@ -32,26 +32,30 @@ def escribeEnDB(conn,tipo,a_movimientos,b_movimientos,exito):
     valores=['\'' + tipo + '\'',str(a_movimientos),str(b_movimientos),str(exito)]
     
     statement = 'INSERT INTO ' + tabla + ' (' + ','.join(columnas) + ') VALUES (' + ','.join(valores) + ')'
-    print statement
+    #print statement
     try:
     	cur.execute(statement) 
     except Exception, e:
         print e.pgerror
     conn.commit()
 
-def testDosMoviendose(conn,limite):
-    tipo='0Quieto'
+
+def test4(conn,limite):
+    """
+    Test movimiento aleatorio total con los dos individuos moviendose
+    """
+    tipo='test4'
     # Situamos a los individuos inicialmente
-    a=Individuo()
-    b=Individuo()
- 
+    a=Individuo(random.randrange(1,borde),random.randrange(1,borde),borde)
+    b=Individuo(random.randrange(1,borde),random.randrange(1,borde),borde)
+
     # Bucle de busqueda
     exito=False
     while (not exito):
-	a.moverACualquiera()
+	a.moverAContigua()
+	b.moverAContigua()
 	if a.movimientos >= limite:
 	    break
-	b.moverACualquiera()
 	if b.movimientos >= limite:
 	    break
 
@@ -62,11 +66,61 @@ def testDosMoviendose(conn,limite):
 
 
 
-def testUnoQuieto(conn,limite):
-    tipo='1Quieto'
+def test3(conn,limite):
+    """
+    Test movimiento aleatorio a las casillas contiguas con un individuo quieto
+    """
+    tipo='test3'
     # Situamos a los individuos inicialmente
-    a=Individuo()
-    b=Individuo()
+    a=Individuo(random.randrange(1,borde),random.randrange(1,borde),borde)
+    b=Individuo(random.randrange(1,borde),random.randrange(1,borde),borde)
+
+    # Bucle de busqueda
+    exito=False
+    while (not exito):
+	a.moverAContigua()
+	if a.movimientos >= limite:
+	    break
+	if (a.x == b.x and a.y == b.y):  # Si estan en la misma casilla... exito
+	    exito=True
+
+    escribeEnDB(conn,tipo,a.movimientos,b.movimientos,exito)
+
+
+
+def test2(conn,limite):
+    """
+    Test movimiento aleatorio total con los dos individuos moviendose
+    """
+    tipo='test2'
+    # Situamos a los individuos inicialmente
+    a=Individuo(random.randrange(1,borde),random.randrange(1,borde),borde)
+    b=Individuo(random.randrange(1,borde),random.randrange(1,borde),borde)
+
+    # Bucle de busqueda
+    exito=False
+    while (not exito):
+	a.moverACualquiera()
+	b.moverACualquiera()
+	if a.movimientos >= limite:
+	    break
+	if b.movimientos >= limite:
+	    break
+	if (a.x == b.x and a.y == b.y):  # Si estan en la misma casilla... exito
+	    exito=True
+
+    escribeEnDB(conn,tipo,a.movimientos,b.movimientos,exito)
+
+
+
+def test1(conn,limite):
+    """
+    Test movimiento aleatorio total con un individuo quieto
+    """
+    tipo='test1'
+    # Situamos a los individuos inicialmente
+    a=Individuo(random.randrange(1,borde),random.randrange(1,borde),borde)
+    b=Individuo(random.randrange(1,borde),random.randrange(1,borde),borde)
  
     # Bucle de busqueda
     exito=False
@@ -82,28 +136,18 @@ def testUnoQuieto(conn,limite):
 
 
 if __name__ == "__main__":
-    # Pillamos los argumentos
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
-    except getopt.error, msg:
-        print msg
-        print "for help use --help"
-        sys.exit(2)
-    # process options
-    for o, a in opts:
-        if o in ("-h", "--help"):
-            print __doc__
-            sys.exit(0)
-    # process arguments
-    for arg in args:
-	pass
+    # Limite del tablero
+    borde=100
 
     # TODO hacerla global
     conn=conectaADB()
     for i in range(10000):
-    	#testUnoQuieto(conn,300000)
 	print i
-    	testDosMoviendose(conn,300000)
+    	test1(conn,20000)
+    	test2(conn,20000)
+    	test3(conn,20000)
+    	test4(conn,20000)
+
 
     
 
